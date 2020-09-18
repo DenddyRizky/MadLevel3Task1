7aa1fa9505b56b90ec5da575ad44a029f862cff7
Denddy <denddyrizky@gmail.com>
2020-09-18 14:35:26 +0200

    Made the layout for the rating screen and shows a random game to rate

--- app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/RatingFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,5 +22,12 @@ class RatingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showRandomAssesableGame()
+    }
+
+    private fun showRandomAssesableGame(){
+        val randomGame = listOf("Red Dead Redemption 2", "Rocket League", "Shadow of the Tombraider").random()
+
+        tvGame.text = randomGame
     }
 }
